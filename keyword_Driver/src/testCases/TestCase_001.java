5812b523eddf3e962cfd9aa832c489f6651ec110
LavetiVenkata <dsatyaveni@gmail.com>
2019-09-20 18:34:34 +0530

    my initial commit

---
+++ keyword_Driver/src/testCases/TestCase_001.java
@@ -0,0 +1,15 @@
+package testCases;
+
+import business_Functions.Common_Keywords;
+
+public class TestCase_001 extends Common_Keywords{
+	
+	
+	public static void main(String[] args) {
+		
+		openBrowser ("chrome");
+		closeBrowser ();
+		
+	}
+
+}
